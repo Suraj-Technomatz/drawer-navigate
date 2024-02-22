cdd0442b387496db95ffe6e529f65543fe8439fa
Suraj Verma <sverma@technomatz.com>
2024-02-22 16:23:32 +0530

    WIP

--- screens/SuggestionScreen/index.jsx
+++ screens/SuggestionScreen/index.jsx
@@ -9,26 +9,22 @@ import {
   ScrollView,
 } from "react-native";
 import { TextInput, Button } from "react-native-paper";
-
+import { Formik } from "formik";
+import * as Yup from "yup"; // If you're using validation
 import axios from "axios";
 import BackgroundImage from "../../components/ImageBackground";
 import { BASE_URL } from "../../utils";
 const image = require("../../assets/back6.jpg");
 
 export default function SuggestionScreen({ navigation }) {
-  const initForm = { name: "", phone: "", suggestion: "" };
-  const [form, setForm] = useState({ ...initForm });
-
-  const onHandleSubmitForm = () => {
-    console.log("=========== onHandleSubmitForm =========", form);
+  const onHandleSubmitForm = (values) => {
     axios
       .post(`${BASE_URL}/v1/suggestion/create`, {
-        ...form,
+        ...values,
         rating: 5,
       })
       .then((res) => {
         console.log("=====================..>>>>>>>>>>", res.data);
-        setForm({ ...initForm });
         navigation.navigate("Home Stack");
       })
       .catch((err) => {
@@ -37,52 +33,66 @@ export default function SuggestionScreen({ navigation }) {
       });
   };
 
-  const isDisable = () => {
-    return !(form.name !== "" && form.phone !== "" && form.suggestion !== "");
-  };
-
-  console.log("========== FORM ========", form);
   return (
     // <BackgroundImage source={image}>
     <View style={styles.container}>
       <ScrollView>
+        <Formik
+          initialValues={{}}
+          validationSchema={Yup.object().shape({
+            name: Yup.string().required("Name is required"),
+            phone: Yup.string().required("Phone is required"),
+            suggestion: Yup.string().required("Suggestion is required"),
+          })}
+          onSubmit={(values, actions) => {
+            onHandleSubmitForm(values);
+          }}
+        >
+          {({ handleChange, handleBlur, handleSubmit, values, errors }) => (
+            <View>
+              <TextInput
+                onChangeText={handleChange("name")}
+                onBlur={handleBlur("name")}
+                value={values.name}
+                placeholder="Name"
+              />
+              {errors.name && (
+                <Text style={{ color: "red" }}>{errors.name}</Text>
+              )}
+
+              <TextInput
+                onChangeText={handleChange("phone")}
+                onBlur={handleBlur("phone")}
+                value={values.phone}
+                placeholder="Phone"
+              />
+              {errors.phone && (
+                <Text style={{ color: "red" }}>{errors.phone}</Text>
+              )}
+
+              <TextInput
+                onChangeText={handleChange("suggestion")}
+                onBlur={handleBlur("suggestion")}
+                value={values.suggestion}
+                placeholder="Suggestion"
+                multiline
+                numberOfLines={4}
+              />
+              {errors.suggestion && (
+                <Text style={{ color: "red" }}>{errors.suggestion}</Text>
+              )}
+              <View style={{ marginVertical: 20 }}>
+                <Button mode="contained" onPress={handleSubmit}>
+                  Submit
+                </Button>
+              </View>
+            </View>
+          )}
+        </Formik>
         <View style={styles.formContainer}>
-          <View style={styles.inputField}>
-            <TextInput
-              label="Name"
-              value={form.name}
-              onChangeText={(text) => setForm({ ...form, name: text })}
-              mode="outlined"
-              style={{ marginBottom: 20 }}
-            />
-          </View>
-          <View style={styles.inputField}>
-            <TextInput
-              label="Phone"
-              value={form.phone}
-              onChangeText={(text) => setForm({ ...form, phone: text })}
-              mode="outlined"
-              style={{ marginBottom: 20 }}
-            />
-          </View>
-          <View style={styles.inputField}>
-            <TextInput
-              label="Suggestion"
-              value={form.suggestion}
-              onChangeText={(text) => setForm({ ...form, suggestion: text })}
-              mode="outlined"
-              multiline
-              numberOfLines={4} // Set the number of lines to display initially
-              style={{ marginBottom: 20 }}
-            />
-          </View>
-          <Button
-            disabled={isDisable()}
-            mode="contained"
-            onPress={onHandleSubmitForm}
-          >
-            Submit
-          </Button>
+          <View style={styles.inputField}></View>
+          <View style={styles.inputField}></View>
+          <View style={styles.inputField}></View>
         </View>
       </ScrollView>
     </View>
